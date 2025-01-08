822726e0162f3e2b23aa8b54b499738b39328176
Matilda Sönnergaard <matilda.sonnergaard@se.experis.com>
2025-01-08 13:11:34 +0100

    Complete core exercise

--- src/main/java/com/booleanuk/Scrabble.java
+++ src/main/java/com/booleanuk/Scrabble.java
@@ -1,12 +1,32 @@
 package com.booleanuk;
 
 public class Scrabble {
+    String word;
     public Scrabble(String word) {
-
+        this.word = word;
     }
 
     public int score() {
-        return -1;
+        int score = 0;
+        for (int i=0; i<word.length(); i++) {
+            String letter = word.substring(i, i + 1);
+            if ("AEIOULNRSTaeioulnrst".contains(letter)) {
+                score += 1;
+            } else if ("DGdg".contains(letter)) {
+                score += 2;
+            } else if ("BCMPbcmp".contains(letter)) {
+                score += 3;
+            } else if ("FHVWYfhvwy".contains(letter)) {
+                score += 4;
+            } else if ("Kk".contains(letter)) {
+                score += 5;
+            } else if ("JXjx".contains(letter)) {
+                score += 8;
+            } else if ("QZqz".contains(letter)) {
+                score += 10;
+            }
+        }
+        return score;
     }
 
 }
